36b7705ec9bc75c099d1b388f76d3b3b9d21cd01
Roman <romazan.26@gmail.com>
2026-04-07 16:39:49 +0300

    Final

---
+++ 115PedalMate/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  115PedalMate
+//
+//  Created by Роман Главацкий on 03.04.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentView()
+}
